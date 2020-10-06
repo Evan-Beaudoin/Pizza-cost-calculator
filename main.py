a285e9c31989530f66121ff566beb825fee11fa3
EvanBeaudoin <evan.beaudoin@ocsbstudent.ca>
2020-10-06 22:04:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+diameter = float(input("Enter the diameter of the pizza you would like (inch): "))
+LABOUR_COST = 0.75
+RENT = 1
+HST_ONTARIO_TAX = 1.13
+materials=  0.5 * diameter
+
+cost_of_pizza_1 = (materials + RENT + LABOUR_COST) * 1.13
+cost_of_pizza_2 = round(cost_of_pizza_1, 2)
+
+print("Your", diameter,"inch pizza costs $",cost_of_pizza_2) 
